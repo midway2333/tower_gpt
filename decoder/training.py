bb65c6490c1037a9353010fe9a5f762ac44e29f5
midway2333 <137980723+midway2333@users.noreply.github.com>
2024-09-28 14:41:25 +0800

    Update training.py

--- decoder/training.py
+++ decoder/training.py
@@ -7,6 +7,7 @@ import sentencepiece as spm
 from galore_torch import GaLoreAdamW8bit
 from torch.utils.tensorboard import SummaryWriter   # type: ignore
 from tfer_chat import *
+from torch import GradScaler, autocast   # type: ignore
 
 class SequentialDataset(Dataset):   # 按顺序遍历文件
     def __init__(self, data_dir, block_size, padding_id):
@@ -58,7 +59,7 @@ class SequentialDataset(Dataset):   # 按顺序遍历文件
     
 
 def train(epoch: int, dataloader: DataLoader, model: nn.Module,   \
-           device: torch.device, writer, rating: float, wr_name):
+           device: torch.device, writer, rating: float, wr_name, steps: int):
 
     """
 
@@ -71,59 +72,70 @@ def train(epoch: int, dataloader: DataLoader, model: nn.Module,   \
     - device (torch.device): 设备类型(CPU或GPU)
     - writer: 用于记录训练日志的对象
     - rating (float): 学习率
+    - wr_name: 训练日志名称
+    - steps: 梯度累积步进
 
     """
-    
-    loss_fn = nn.CrossEntropyLoss()  # 交叉熵损失函数
 
-    model.train()  # 设置为训练模式
+    loss_fn = nn.CrossEntropyLoss()   # 交叉熵损失函数
+    accumulation_steps = steps   # 设置累积步数
+    scaler = GradScaler()
+
+    model.train()   # 设置为训练模式
     optimizer = GaLoreAdamW8bit(model.parameters(), lr=rating,   \
                     betas=(0.9, 0.999), eps=1e-8, weight_decay=0.01)
 
-    train_steps = 0  # 初始化训练步数
+    train_steps = 0   # 初始化训练步数
 
     for i in range(epoch):
 
         epoch_loss = 0
+        print('start step')
 
-        for x, y in dataloader:
+        for step, (x, y) in enumerate(dataloader):   # 生成步进索引
 
-            x, y = x.to(device).long(), y.to(device).long()  # 将数据移动到指定设备上
-            pred = model(x)
-            pred = pred.view(-1, pred.size(-1))
-            # 将 pred 重新形状为 [batch_size * sequence_length, vocab_size]
+            x, y = x.to(device).long(), y.to(device).long()   # 将数据移动到指定设备上
 
-            y = y.view(-1)
-            # 将 y 重新形状为 [batch_size * sequence_length]
+            with autocast(device_type='cuda' if torch.cuda.is_available() else 'cpu'):
+            # 自动混合精度
 
-            loss = loss_fn(pred, y)     # 计算损失
-            loss.backward()             # 反向传播，计算梯度
+                pred = model(x)
+                pred = pred.view(-1, pred.size(-1))
+                # 将 pred 重新形状为 [batch_size * sequence_length, vocab_size]
 
-            torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
-            # 梯度裁剪
+                y = y.view(-1)
+                # 将 y 重新形状为 [batch_size * sequence_length]
 
-            optimizer.step()            # 更新模型参数
-            optimizer.zero_grad()       # 清空梯度
+                loss = loss_fn(pred, y) / accumulation_steps   # 计算损失
 
-            epoch_loss += loss.item()   # 累加batch损失
-            avg_epoch_loss = epoch_loss / len(dataloader)   # 计算平均epoch损失
+            scaler.scale(loss).backward()
+            # 使用混合精度来缩放损失,反向传播
+            
+            if (step + 1) % accumulation_steps == 0:
+
+                torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
+                # 梯度裁剪
+
+                scaler.step(optimizer)   # 更新模型参数
+                scaler.update()          # 调整缩放比例
+
+            epoch_loss += loss.item()    # 累加batch损失
+            avg_epoch_loss = (epoch_loss / len(dataloader)) * accumulation_steps
+            # 计算平均epoch损失
 
         if (i + 1) % 10 == 0:
-            if isinstance(loss, torch.Tensor):
-                print(f'Epoch: {i + 1}; avg_Loss: {avg_epoch_loss}')
-            else:
-                print(f'Epoch: {i + 1}; avg_Loss: {avg_epoch_loss}')
+            print(f'Epoch: {i + 1}; avg_Loss: {avg_epoch_loss}')
         # 打印当前的epoch与loss
 
         train_steps += 1
-        writer.add_scalar(wr_name, avg_epoch_loss, train_steps)  # 记录训练损失
+        writer.add_scalar(wr_name, avg_epoch_loss, train_steps)   # 记录训练损失
     
 if __name__ == "__main__":
 
-    data_dir = 'nptxt'
+    data_dir = 'output-np'
     block_size = 512
-    batch_size = 4
-    # dataset设置
+    batch_size = 3
+    # dataset/dataloader设置
 
     sp = spm.SentencePieceProcessor()
     sp.load('work\\tokenizer\\spm_dict.model')   # type: ignore
@@ -135,12 +147,14 @@ if __name__ == "__main__":
     # 设备获取
 
     model = transformer(vocab_size=vocab_size, padding_idx=padding_id)
+    model.load_state_dict(torch.load('test_tower_alpha', weights_only=True))
     # 模型设置
 
-    epoch = 300
-    rating = 0.0001
+    epoch = 5
+    rating = 0.0004
+    step = 32
     writer = SummaryWriter('tr_logs')
-    name = 'train_loss_0.0001'
+    name = 'test_tower'
     # 训练设置
 
 
@@ -149,7 +163,7 @@ if __name__ == "__main__":
                                 num_workers=4, pin_memory=True)
     
 
-    train(epoch=epoch, dataloader=dataloader, model=model, \
-            device=device, writer=writer, rating=rating, wr_name=name)
+    train(epoch=epoch, dataloader=dataloader, model=model,   \
+            device=device, writer=writer, rating=rating, wr_name=name, steps=step)
 
-    torch.save(model.state_dict(), 'test7m_0.001.pth')
+    torch.save(model.state_dict(), 'test_tower_alpha')
